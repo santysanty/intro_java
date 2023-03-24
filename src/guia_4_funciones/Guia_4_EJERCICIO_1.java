c832e5d79b2112afc5e07e6574512337aff38459
“santysanty” <dsanty0@gmail.com>
2023-03-24 00:10:55 -0300

    " funciones: ejercicio 1 terminado "

--- src/guia_4_funciones/Guia_4_EJERCICIO_1.java
+++ src/guia_4_funciones/Guia_4_EJERCICIO_1.java
@@ -1,4 +1,4 @@
-/* GUIA 4 - FUNCIONES
+/* GUIA 4 - FUNCIONES- EJERCICIO_1(UNO)
  * Crea una aplicación que le pida dos números al usuario y este pueda elegir 
  *entre sumar, restar, multiplicar y dividir. La aplicación debe tener una
  *función para cada operación matemática y deben devolver sus resultados para 
@@ -17,14 +17,30 @@ public class Guia_4_EJERCICIO_1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int num1,num2;
+        String salir;
+        int a,b,operacion;
          Scanner leer = new Scanner(System.in);
+         do{
          System.out.println("Ingrese un Numero:");
-         num1=leer.nextInt();
+         a=leer.nextInt();
          System.out.println("Ingrese un Numero:");
-         num2=leer.nextInt();
+         b=leer.nextInt();
+         System.out.println("Realizar la operacion:");
+         System.out.println("1-suma..2-resta.. 3-multiplicacion..4- division:");
+
+         operacion=leer.nextInt();
          
-         
+         switch(operacion){
+             case 1: System.out.println("La suma es:"+suma(a,b)); break;
+             case 2: System.out.println("La resta es:"+resta(a,b)); break;
+             case 3: System.out.println("La multiplicacion es:"+multiplicacion(a,b)); break;
+             case 4: System.out.println("La division es:"+dividir(a,b)); break;
+             default:  System.out.println(" opcion Incorrecta..");
+         }
+       
+         System.out.print("Desea Salir(SI/NO):");
+         salir=leer.next();
+         }while(salir=="SI"); 
          
     }
     public static int suma(int a,int b){
