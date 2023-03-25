0db3d18171f06ef1118ee31282ca00c2557ebd8d
“santysanty” <dsanty0@gmail.com>
2023-03-25 08:57:33 -0300

    " cilco control While: ejercicio 5 terminado << Ingrese 2 numero para sumar. hasta que supere el valor inicial del primero>> "

---
+++ src/Guia_3_unidad_1/Guia_3_EJERCICIO_5.java
@@ -0,0 +1,28 @@
+/*
+ ** Escriba un programa en el cual se ingrese un valor límite positivo, y a 
+ *continuación solicite números al usuario hasta que la suma de los números 
+*introducidos supere el límite inicial
+ */
+package Guia_3_unidad_1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Daniel Coria
+ */
+public class Guia_3_EJERCICIO_5 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+       int numeroPositivo, numeroSiguiente, suma;
+        System.out.println("Ingrese un numero positivo");
+    	numeroPositivo= leer.nextInt();
+         suma=0;
+        while(suma< numeroPositivo ){ //
+         System.out.println("Ingrese un numero para sumar. hasta que supere el valor inicial.");
+         numeroSiguiente=leer.nextInt();
+         suma=suma+numeroSiguiente;
+        }
+         System.out.println("La suma es :"+suma);
+  }
+}
